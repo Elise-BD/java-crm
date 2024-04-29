e114de2c2c723193c70fb45887d11fd7eaa40eee
Elise B. Dirheimer <elise.dirheimer@gmail.com>
2024-04-29 10:56:27 +0200

    Init

---
+++ src/main/java/com/example/javacrm/JavaCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrmApplication.class, args);
+	}
+
+}
