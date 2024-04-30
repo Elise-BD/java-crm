8dba60189bf28a62f0133aef552b8344f0b72b9e
Elise B. Dirheimer <elise.dirheimer@gmail.com>
2024-04-30 11:56:54 +0200

    entities utilisateur + projet + tache + enums

--- src/main/java/com/example/javacrm/dal/entities/Utilisateur.java
+++ src/main/java/com/example/javacrm/dal/entities/Utilisateur.java
@@ -3,6 +3,9 @@ package com.example.javacrm.dal.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Entity
 public class Utilisateur {
@@ -13,4 +16,20 @@ public class Utilisateur {
 
     @Column(name = "reference")
     private String reference = "U-"+id;
+
+    @Column(name = "nom", nullable = false)
+    private String nom;
+
+    @Column(name = "prenom", nullable = false)
+    private String prenom;
+
+    @Column(name = "fonction")
+    private String fonction;
+
+    @OneToMany(mappedBy = "utilisateur", orphanRemoval = true)
+    private List<Tache> taches = new ArrayList<>();
+
+    public void setTaches(List<Tache> taches) {
+        this.taches = taches;
+    }
 }
---
+++ src/main/java/com/example/javacrm/dal/enums/StatutProjet.java
@@ -0,0 +1,9 @@
+package com.example.javacrm.dal.enums;
+
+public enum StatutProjet {
+    CREE,
+    EN_COURS,
+    EN_RETARD,
+    TERMINE,
+    ANNNULE;
+}
